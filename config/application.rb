b671f849f101dd8938f4b6a15042fd31da3bbeb2
太田良司 <ootaryouji@ryouji.local>
2021-02-21 21:46:51 +0900

    投稿時刻の日本時間への修正

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,9 @@ module ChatApp
       g.javascripts false
       g.helper false
       g.test_framework false
+      config.load_defaults 6.0 
+      config.i18n.default_locale = :ja
+      config.time_zone = 'Tokyo'
     end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
